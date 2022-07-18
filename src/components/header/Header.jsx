6a16dec83ec44def0a625fa2119b48ff85916d5f
legit0108 <legitgamer2345@gmail.com>
2022-07-19 00:35:07 +0530

    made header responsive,started floating nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
-import ME from '../../assets/meCropped.png'
 
 function Header() {
   return (
@@ -13,11 +12,6 @@ function Header() {
         <h5 className="text-light">Upcoming Technology Developer Intern at D.E. Shaw</h5>
         <CTA/>
         <HeaderSocials/>
-
-        <div className="me">
-          <img src={ME} alt="me"></img>
-        </div>
-        
         <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>
