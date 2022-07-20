96993b2debcc32bca74935e1d99b4a83c2473da4
legit0108 <legitgamer2345@gmail.com>
2022-07-20 19:51:21 +0530

    finished services component and started portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
 
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className=".container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt=""></img>
+          </div>
+          <h3>Browse Movies</h3>
+          <a href="https://github.com/legit0108/movies-app-final" className="btn" target="_blank">Github</a>
+          <a href="https://browse-movies.netlify.app/" className="btn btn-primary" target="_blank">Live Demo</a>                                            
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt=""></img>
+          </div>
+          <h3>Browse Movies</h3>
+          <a href="https://github.com/legit0108/movies-app-final" className="btn" target="_blank">Github</a>
+          <a href="https://browse-movies.netlify.app/" className="btn btn-primary" target="_blank">Live Demo</a>                                            
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt=""></img>
+          </div>
+          <h3>Browse Movies</h3>
+          <a href="https://github.com/legit0108/movies-app-final" className="btn" target="_blank">Github</a>
+          <a href="https://browse-movies.netlify.app/" className="btn btn-primary" target="_blank">Live Demo</a>                                            
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt=""></img>
+          </div>
+          <h3>Browse Movies</h3>
+          <a href="https://github.com/legit0108/movies-app-final" className="btn" target="_blank">Github</a>
+          <a href="https://browse-movies.netlify.app/" className="btn btn-primary" target="_blank">Live Demo</a>                                            
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 function Services() {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Programming</h2>
+
+      <div className="container services__container">
+         <article className="service">
+           <div className="service__head">
+            <h3>Web Development</h3>
+           </div>
+           <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Proficient In Front-End Development</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Proficient With Using ReactJS , JavaScript , HTML , CSS</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Currently Learning Back-End Development</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Currently Learning MongoDB , Django</p>
+            </li>
+           </ul>
+         </article>
+         <article className="service">
+           <div className="service__head">
+            <h3>DSA And Problem Solving</h3>
+           </div>
+           <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Proficient In DSA And Problem Solving</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Solved 1500+ Problems Across Various Platforms</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Knight At LeetCode With A Contest Rating of 2000+</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>4* Coder At CodeChef</p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon"/>
+              <p>Specialist At Codeforces</p>
+            </li>
+           </ul>
+         </article>
+      </div>
+    </section>
   )
 }
 
