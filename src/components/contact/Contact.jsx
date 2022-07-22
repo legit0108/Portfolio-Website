e57963286d14e0be03a4b751d9289eb7e64ea092
legit0108 <legitgamer2345@gmail.com>
2022-07-23 00:31:37 +0530

    Updated Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsWhatsapp} from 'react-icons/bs'
 
 function Contact() {
   return (
@@ -14,23 +16,32 @@ function Contact() {
             <MdOutlineEmail/>
             <h4>Email</h4>
             <h5>lakshitpandey02@gmail.com</h5>
-            <a href="matilto:lakshitpandey02@gmail.com">Send A Message</a>
+            <a href="mailto:lakshitpandey02@gmail.com" target="_blank">Send An Email</a>
           </article>
+          
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <BsLinkedin/>
             <h4>LinkedIn</h4>
             <h5>Lakshit Pandey</h5>
-            <a href="matilto:lakshitpandey02@gmail.com">Send A Message</a>
+            <a href="https://www.linkedin.com/in/lakshit-pandey-66a0591b3/" target="_blank">Connect On LinkedIn</a>
           </article>
+
           <article className="contact__option">
-            <MdOutlineEmail/>
-            <h4>Email</h4>
-            <h5>lakshitpandey02@gmail.com</h5>
-            <a href="matilto:lakshitpandey02@gmail.com">Send A Message</a>
+            <BsWhatsapp/>
+            <h4>WhatsApp</h4>
+            <h5>+91 7995866127</h5>
+            <a href="https://wa.me/917995866127" target="_blank">Send A Message</a>
           </article>
         </div>
+
         {/* END OF CONTACT OPTIONS */}
-        <form></form>
+
+        <form action="">
+          <input type="text" name="name" placeholder='Your Full Name' required></input>
+          <input type="email" name="email" placeholder="Your Email" required></input>
+          <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
+          <button type="submit" className="btn btn-primary">Send Message</button>    
+        </form>
       </div>
     </section>
   )
