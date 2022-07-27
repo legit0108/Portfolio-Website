0d54d8b4b478576a3df86765fb30f7f5713d5810
legit0108 <legitgamer2345@gmail.com>
2022-07-27 17:05:01 +0530

    updated resume file

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/Lakshit Pandeys Resume for Summer Internships.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">Download CV</a>
+      <a href={CV} download="Lakshit Pandey's Resume for Summer Internships" className="btn">Download CV</a>
       <a href="#contact" className="btn btn-primary">Contact Me</a> 
     </div>
   )
