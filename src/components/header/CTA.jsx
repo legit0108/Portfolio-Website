77cd7f341d6cefb78e9ed339b587b0404352b45d
legit0108 <legitgamer2345@gmail.com>
2022-12-21 12:17:13 +0530

    Update: 21-12-2022

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/Lakshit Pandey\'s resume for SDE-1 roles.pdf'
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download="Lakshit Pandey's Resume for Summer Internships" className="btn">Download CV</a>
+      <a href={CV} download="Lakshit Pandey's resume for SDE-1 roles" className="btn">Download CV</a>
       <a href="#contact" className="btn btn-primary">Contact Me</a> 
     </div>
   )
