8c489551d2d5ae7a4266d517f31670d9bb181bb8
legit0108 <legitgamer2345@gmail.com>
2023-08-11 23:11:31 +0530

    Updated Header, about section and resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,8 +35,6 @@ function About() {
             CSE student at Thapar Institute of Engineering and Technology, Patiala. I like coding and problem-solving, 
             I'm a Knight at LeetCode with a contest rating of 2000+, a 4* coder at CodeChef and
             a Specialist at Codeforces. I am comfortable with Full-Stack web development and have worked with libraries and frameworks like React-Redux and Django. 
-            I am interested in applying for any full-time positions for SDE-1 roles, if you know of any opportunities like this, I’d love to chat. 
-            I’ve uploaded my resume above for your consideration, and you can reach me directly via LinkedIn or Email.
           </p>
         </div>
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Lakshit Pandey</h1>
-        <h5 className="text-light">SDE Summer Intern at D. E. Shaw & Co. (SIP’23)</h5>
+        <h5 className="text-light">Former SDE Summer Intern at D. E. Shaw & Co. ’23</h5>
         <CTA/>
         <HeaderSocials/>
         <a href="#contact" className="scroll__down">Scroll down</a>
